35fd9334fa0ecfa864b28d132f5ec1614c778b34
mojtabababiker <mojmohammad98@gmail.com>
2023-11-20 08:23:48 +0200

    Add the MYSQL server setup files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,6 +8,5 @@ CREATE USER
        IF NOT EXISTS 'hbnb_dev'@'localhost'
        IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,6 +8,5 @@ CREATE USER
        IF NOT EXISTS 'hbnb_test'@'localhost'
        IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL ON hbnb_test_db TO 'hbnb_test'@'localhost';
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
