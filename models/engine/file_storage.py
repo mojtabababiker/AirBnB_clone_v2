273523ba85db652f8c3a9b015b802232f9edd9ab
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-11-21 05:53:21 -0800

    file-storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,120 +1,46 @@
 #!/usr/bin/python3
-"""
-A Storage Engine based on file storage mechanisim and the JSON format.
-This model is used to create a presistent copy of all the BnB models objects
-in the form of a json file
-"""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
-import os.path
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
 
 class FileStorage:
-    """
-    Syntax:
-        FileStorage()
-
-    Description:
-        Creates a presistent copy of the BnB models objects in the form of
-        json file
-
-        FileStorage.all(self):
-            returns a dicitionery consists of all the objects of the Bnb models
-        FileStorage.new(self, obj):
-            Update the objects dictionery with the
-            <obj.__class__.__name__>.<obj.id> as the key
-            and obj.to_dict() as its value
-
-        FileStorage.save(self):
-            Serializes the objects dictionery to a JSON file
-
-        FileStorage.reload(self):
-            Deserializes the JSON file to the objects dictionery
-    """
-
-    __file_path = os.path.join(os.path.dirname(__file__), 'file.json')
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """
-        all    return a dictionery consists of all the objects instances of
-               the BnB models
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
 
-        Syntax:
-            FileStorage.all()
-
-        Return:
-            A dictionery contains BnB models instances
-        """
-
-        return self.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            return {key: obj for key, obj in FileStorage.__objects.items() if isinstance(obj, cls)}
+        return FileStorage.__objects
 
     def new(self, obj):
-        """
-        new    add the obj information to the BnB objects dictionery
-
-        Syntax:
-            FileStorage.new(self, obj)
-        """
-
-        key = obj.to_dict()['__class__'] + '.' + obj.id
-        self.__objects[key] = obj
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """
-        save   save the BnB objects dictionery into a JSON file
-
-        Syntax:
-            FileStoarge.save()
-
-        Descriptons:
-            `save` saves all the BnB objects instances to a JSON file, in the
-             form of json key:value pairs.
-             If the file dose not exists the `save` will creates a new one, if
-             it's already exist it will be over-written
-        """
-
-        with open(self.__file_path, 'w') as f:
-            data = {key: val.to_dict() for key, val in self.__objects.items()}
-            json.dump(data, f)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """
-        reload    load the contents of the json file to the BnB objects
-                  instances dictionery
-
-        Syntax:
-            FileStorage.load()
-
-        Descriptions:
-            `reload` get the contents of the json file and converts them into
-             python dictionery, and the value of the BnB objects instances
-             dictionery will be updated with it.
-        """
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+        """Loads storage dictionary from file"""
         try:
-            with open(self.__file_path, 'r') as f:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    class_name = val.get('__class__')
-                    if class_name in classes:
-                        self.__objects[key] = classes[class_name](**val)
+                    self.all()[key] = BaseModel(**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """Delete obj from __objects if it's inside"""
-        if obj is not None:
-            key = obj.to_dict()['__class__'] + '.' + obj.id
-            self.__objects.pop(key, None)
--- models/engine/main_delete.py
+++ models/engine/main_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ Test delete feature
 """
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from models.engine.file_storage import FileStorage
 from models.state import State
 import uuid
