9323bc6574abcb4ac960b9a1009f0b257ff5f0cc
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-11-22 14:32:46 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -8,8 +8,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship("Place", secondary="place_amenity", back_populates="amenities")
-
     def __init__(self, *args, **kwargs):
         """ Initiate the instance with some default values and call the
         super init to complete the initiate
