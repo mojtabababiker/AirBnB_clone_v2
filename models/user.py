056c567f5bb1505a5e6e530475f55500d1e15037
mojtabababiker <mojmohammad98@gmail.com>
2023-11-22 21:56:11 +0200

    Edit typo on the user models, wrone datataype

--- models/user.py
+++ models/user.py
@@ -12,8 +12,8 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", backref="user",
                           cascade="all, delete-orphan")
@@ -26,8 +26,6 @@ class User(BaseModel, Base):
         """
         self.email = ""
         self.password = ""
-        self.first_name = ""
-        self.last_name = ""
         super().__init__(*args, **kwargs)
 
 
