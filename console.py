d96d66be20cf0bf15b1dbf1d1c82ba26fe38bc75
mojtabababiker <mojmohammad98@gmail.com>
2023-11-22 07:27:50 +0200

    Updare console so that the newly created object get added to the storage.__objects/.__session

--- console.py
+++ console.py
@@ -156,9 +156,8 @@ class HBNBCommand(cmd.Cmd):
             except ValueError:
                 continue
 
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
