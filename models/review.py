72948f8e197050b64a2ce80fc46f0a09eec87655
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-11-22 13:32:13 -0800

    Review Correction

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class Review(BaseModel, Base):
@@ -11,12 +12,6 @@ class Review(BaseModel, Base):
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place = relationship("Place", backref="reviews",
-                             cascade="all, delete-orphan")
-        user = relationship("User", backref="reviews",
-                            cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """ Initiate the instance with some default values and call the
