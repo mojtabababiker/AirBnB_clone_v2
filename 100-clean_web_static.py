eb3603220247cc2613b97cec50c0e60979f31be6
mojtabababiker <mojmohammad98@gmail.com>
2024-01-10 20:54:51 -0800

    Add do_clean function

---
+++ 100-clean_web_static.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+"""
+A fabfile to prepare the static file for deploying
+"""
+from fabric.api import *
+from datetime import datetime
+import os
+import os.path
+
+
+
+env.hosts = []
+def do_pack():
+    """
+    function which generates a .tgz archive from the contents
+    of web-static in order to prepare it to be pushed to the
+    server
+    """
+    date = datetime.now()
+    name = date.strftime('%Y%m%d%H%M%S')
+
+    if not os.path.isdir('versions'):
+        os.mkdir('versions')
+
+    result = local(f"tar -cvzf versions/web_static_{name}.tgz web_static")
+
+    if result.failed:
+        return None
+
+    size = os.path.getsize(f"versions/web_static_{name}.tgz")
+    print(f"web_static packed: versions/web_static_{name}.tgz -> {size}Bytes")
+
+    return f"versions/web_static_{name}.tgz"
+
+
+def do_deploy(arhcive_path):
+    """
+    push and uncompress the archive file in the `archive_path` to the server/s
+    """
+    if archive_path is None or not os.path.isfile(archive_path):
+        return False
+    result = put(local_path=archive_path, remote_path='/temp')
+    if result.failed:
+        return False
+    name = os.path.basename(archive_path).split('.')[0]
+
+    result = run(f"mkdir -p /data/web_static/releases/{name}")
+    if result.failed:
+        return False
+    result = run(f"tar -xzf /tmp/web_static_{name}.tgz -C /data/web_static/releases/{name}")
+    if result.failed:
+        return False
+
+    result = run(f"mv /data/data/web_static/releases/{name}/web_static/*" +
+                 "  /data/data/web_static/releases/{name}/")
+    if result.failed:
+        return False
+
+    result = run(f"rm  -rf /data/data/web_static/releases/{name}/web_static")
+    if result.failed:
+        return False
+
+    result = run(f"rm -f /tmp/web_static_{name}.tgz")
+    if result.failed:
+        return False
+
+    result = run("rm -rf /data/web_static/current")
+    if result.failed:
+        return False
+
+    result = run(f"ln -snf /data/web_static/releases/web_static_{name}/ /data/web_static/current")
+    if result.failed:
+        return False
+
+    print("New version deployed!")
+    return True
+
+
+def deploy():
+    """
+    handle the deploy proccess from the start to fully deploying
+    statics into server/s
+    """
+    ar_path = do_pack()
+    if not ar_path:
+        return False
+
+    return do_deploy(ar_path)
+
+
+def do_clean(number=0):
+    """
+    delete all the out-of-date archives from versions and from
+    /data/web_static/releases
+    """
+    if number == 0:
+        number == 1
+    else if number > 0:
+        return
+
+    ar_list = os.listdir('versions')
+
+    for i in range(len(ar_list) - 1 - number):
+        name = ar_list[i].split('.')[0]
+        local(f"rm -f versions/{ar_list[i]}")
+        run(f"rm -fr /data/web_static/releases/{name}")
