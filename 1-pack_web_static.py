d7529594627317fc58e54dd5822b31f7c8237979
mojtabababiker <mojmohammad98@gmail.com>
2024-01-10 19:27:29 -0800

    Updating the pack function inhanced code and fix some bugs

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-""" Fabric script that pack and prepairs the static contents to be uploaded """
-
+"""
+A fabfile to prepare the static file for deploying
+"""
 from fabric.api import *
+from datetime import datetime
 import os
 import os.path
 
 
 def do_pack():
     """
-    Pack the contect of AirBnB static ino .tar
+    function which generates a .tgz archive from the contents
+    of web-static in order to prepare it to be pushed to the
+    server
     """
-    if not os.path.isdir("./versions"):
-        os.mkdir("./versions")
-    name = local("date +%Y%m%d%H%m%S", capture=True)
-    result = local(f"tar -cvzf versions/web_static_{name}.tgz web_static",
-                   capture=True)
+    date = datetime.now()
+    name = date.strftime('%Y%m%d%H%M%S')
+
+    if not os.path.isdir('versions'):
+        os.mkdir('versions')
+
+    result = local(f"tar -cvzf versions/web_static_{name}.tgz web_static")
 
     if result.failed:
         return None
-    return "versoins/web_static_{name}.tgz"
+
+    size = os.path.getsize(f"versions/web_static_{name}.tgz")
+    print(f"web_static packed: versions/web_static_{name}.tgz -> {size}Bytes")
+
+    return f"versions/web_static_{name}.tgz"
