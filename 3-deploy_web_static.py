c940156fa2c4ed21bac6433711245d77104d390f
mojtabababiker <mojmohammad98@gmail.com>
2024-01-13 21:39:02 +0200

    Edit full deploy configuration file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -43,43 +43,25 @@ def do_deploy(archive_path):
     """
     if archive_path is None or not os.path.isfile(archive_path):
         return False
-    result = put(local_path=archive_path, remote_path='/tmp/')
-    if result.failed:
-        return False
-    name = os.path.basename(archive_path).split('.')[0]
+    try:
+        result = put(local_path=archive_path, remote_path='/tmp/')
+        name = os.path.basename(archive_path).split('.')[0]
 
-    result = run("mkdir -p /data/web_static/releases/{}/".format(name))
-    if result.failed:
-        return False
-    result = run("tar -xzf /tmp/{}.tgz".format(name) +
-                 " -C /data/web_static/releases/{}/".format(name))
-    if result.failed:
-        return False
-
-    result = run("cp -rf /data/web_static/releases/{}/web_static/*".format(
-        name) + "  /data/web_static/releases/{}/".format(name))
-    if result.failed:
-        return False
-
-    result = run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-    if result.failed:
+        result = run(f"mkdir -p /data/web_static/releases/{name}/")
+        result = run(f"tar -xzf /tmp/{name}.tgz" +
+                     f" -C /data/web_static/releases/{name}/")
+        result = run(f"cp -rf /data/web_static/releases/{name}/web_static/*"
+                     + f"  /data/web_static/releases/{name}/")
+        result = run(f"rm -rf /data/web_static/releases/{name}/web_static")
+        result = run(f"rm -rf /tmp/{name}.tgz")
+        result = run("rm -rf /data/web_static/current")
+        result = run(f"ln -s /data/web_static/releases/{name}/" +
+                     "  /data/web_static/current")
+        # print("New version deployed!")
+        return True
+    except Exception:
         return False
 
-    result = run("rm -rf /tmp/{}.tgz".format(name))
-    if result.failed:
-        return False
-
-    result = run("rm -rf /data/web_static/current")
-    if result.failed:
-        return False
-
-    result = run("ln -s /data/web_static/releases/{}/".format(name) +
-                 "  /data/web_static/current")
-    if result.failed:
-        return False
-
-    print("New version deployed!")
-    return True
 
 def deploy():
     """
